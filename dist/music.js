7730e3f686d1f156708b1141831fc90e2a2f29c9
changxian_gu <changxian_gu@stu.xidian.edu.cn>
2022-12-07 15:24:16 +0800

    Site updated: 2022-12-07 15:24:16

---
+++ dist/music.js
@@ -0,0 +1,25 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        name: "Rain Mood",
+        artist: 'null',
+        url: 'http://music.163.com/song/media/outer/url?id=29818028.mp3',
+        cover: 'https://p1.music.126.net/jsa4j2ZI3hzfdZbYT1yHXw==/109951163018453563.jpg?param=200y200',
+      },
+	 {
+        name: "Rain and Thunder",
+        artist: 'null',
+        url: 'http://music.163.com/song/media/outer/url?id=399343470.mp3',
+        cover: 'https://p1.music.126.net/jsa4j2ZI3hzfdZbYT1yHXw==/109951163018453563.jpg?param=200y200',
+      },
+	 {
+        name: "10 Hz Alpha Thunderstorm",
+        artist: 'null',
+        url: 'http://music.163.com/song/media/outer/url?id=32070215.mp3',
+        cover: 'https://p1.music.126.net/jsa4j2ZI3hzfdZbYT1yHXw==/109951163018453563.jpg?param=200y200',
+      },
+    ]
+});
